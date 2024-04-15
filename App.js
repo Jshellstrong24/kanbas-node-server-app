1a50914cbe5dc0e8dc77b515d8c2a11aa2878072
Jshellstrong24 <144293445+Jshellstrong24@users.noreply.github.com>
2024-04-15 17:25:38 -0400

    domain

--- App.js
+++ App.js
@@ -16,12 +16,12 @@ mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    origin: 'https://661d7cf1e6a5ad000743a235--quiet-banoffee-b41e15.netlify.app'//'process.env.FRONTEND_URL'
 }
 ));
 
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET,
+    secret: 'process.env.SESSION_SECRET',
     resave: false,
     saveUninitialized: false,
   };
@@ -30,7 +30,7 @@ const sessionOptions = {
     sessionOptions.cookie = {
       sameSite: "none",
       secure: true,
-      domain: process.env.HTTP_SERVER_DOMAIN,
+      domain: 'kanbas-node-server-app-1-422p.onrender.com'//process.env.HTTP_SERVER_DOMAIN,
     };
   }
   
